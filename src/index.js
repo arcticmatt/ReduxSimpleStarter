ea3cc6980e1b056173fcad37890ccb08e80654cc
Matt Lim <mlim@caltech.edu>
2017-06-28 16:16:59 -0700

    S1L14. Create component files. Add Youtube API key, and npm install youtube api. Render search bar.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 // Notes
 // ReactDOM - interact with DOM
 // React - create and manage components
+// Component - function or object that returns HTML
+// One component per file!
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD-zHGq3FkbaTqgiohr7aU7P6XsP5P2h1c';
 
 // Create a new component. This component should produce
 // some HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
