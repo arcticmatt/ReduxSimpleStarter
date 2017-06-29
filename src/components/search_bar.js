fcbc27207c15e7eb555de95a8f9771f56c345763
Matt Lim <mlim@caltech.edu>
2017-06-28 18:00:43 -0700

    Finish Section 1

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const SearchBar = () => {
-  return <input />
-};
+// Class component vs. Function component
+// This is a class component
+class SearchBar extends Component {
+  // Note: functional components don't have state
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'yoyoyo' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value})} />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
