0e588aaf33730afcdd1a558cd9f56f65a9e254cc
Matt Lim <mlim@caltech.edu>
2017-06-29 13:09:40 -0700

    Finish section 2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,15 +7,20 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: 'yoyoyo' };
+    this.state = { term: '' };
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 
   render() {
     return (
-      <div>
+      <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={e => this.setState({ term: e.target.value})} />
+          onChange={e => this.onInputChange(e.target.value)} />
       </div>
     );
   }
